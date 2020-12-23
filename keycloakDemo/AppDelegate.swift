5584f34d9e0cf653fb73a8e6a1cd41be28190e28
Aleksandar Geyman <ageyman@appolica.com>
2020-12-23 13:25:04 +0200

    initial setup

--- keycloakDemo/AppDelegate.swift
+++ keycloakDemo/AppDelegate.swift
@@ -6,13 +6,26 @@
 //
 
 import UIKit
+import AppAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    var currentAuthorizationFlow: OIDExternalUserAgentSession? {
+        didSet {
+            print("did setup auth flow")
+        }
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        return true
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if let authFlow = currentAuthorizationFlow {
+            authFlow.resumeExternalUserAgentFlow(with: url)
+        }
         return true
     }
 }
--- keycloakDemo/ViewController.swift
+++ keycloakDemo/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import AppAuth
+
+struct AuthConstants {
+    static let authorizationEndpoint = "https://localhost:8443/auth/realms/index-labs"
+    static let tokenEndpoint = "https://localhost:8443/auth/realms/index-labs/protocol/openid-connect/token"
+    static let redirectURI = "https://www.example.com/redirect"
+}
 
 class ViewController: UIViewController {
+    private var authState: OIDAuthState?
+    private var authConfiguration: OIDServiceConfiguration? {
+        guard let authURL = URL(string: AuthConstants.authorizationEndpoint),
+              let tokenURL = URL(string: AuthConstants.tokenEndpoint) else { return nil}
+
+        return OIDServiceConfiguration(authorizationEndpoint: authURL, tokenEndpoint: tokenURL)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setDelegate()
     }
     
-    @IBAction func didTapOnAuthButton(_ sender: Any) {
+    @IBAction private func didTapOnAuthButton(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.currentAuthorizationFlow = OIDAuthState.authState(byPresenting: makeAuthRequest(), presenting: self, callback: { [unowned self] authState, error in
+            if let authState = authState {
+                self.authState = authState
+            } else {
+                print(#function, error?.localizedDescription)
+                self.authState = nil
+            }
+        })
+    }
+    
+    private func makeAuthRequest() -> OIDAuthorizationRequest {
+        return OIDAuthorizationRequest(configuration: authConfiguration!,
+                                       clientId: "mobile",
+                                       scopes: ["openid", "email", "roles"],
+                                       redirectURL: URL(string: AuthConstants.redirectURI)!,
+                                       responseType: "code",
+                                       additionalParameters: nil)
+    }
+
+    private func setDelegate() {
+        authState?.stateChangeDelegate = self
+    }
+}
+
+extension ViewController: OIDAuthStateChangeDelegate {
+    func didChange(_ state: OIDAuthState) {
 
     }
 }
