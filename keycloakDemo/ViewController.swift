e257b532f1e9c4e14cf216b03c3181c0147d70fe
Aleksandar Geyman <ageyman@appolica.com>
2020-12-23 12:48:58 +0200

    initial commit

---
+++ keycloakDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  keycloakDemo
+//
+//  Created by Aleksandar Geyman on 23.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func didTapOnAuthButton(_ sender: Any) {
+
+    }
+}
